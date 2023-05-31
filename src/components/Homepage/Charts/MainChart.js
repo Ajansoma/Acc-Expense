390ca6dfb96ad9f61b47a8e3085374a81bf426b9
khadijahaabubakar@gmail.com <khadijahaabubakar@gmail.com>
2023-05-31 15:43:12 +0000

    updated

--- src/components/Homepage/Charts/MainChart.js
+++ src/components/Homepage/Charts/MainChart.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js/auto';
 import useCalc from '../../../hooks/useCalc';
 import styles from './MainChart.module.css';
 
